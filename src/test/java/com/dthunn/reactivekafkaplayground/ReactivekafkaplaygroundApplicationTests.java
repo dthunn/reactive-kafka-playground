dfdf45968f2195119f61f189e82acbcf18e58e11
Dylan Thunn <49996239+dthunn@users.noreply.github.com>
2025-04-29 15:20:27 -0600

    init

---
+++ src/test/java/com/dthunn/reactivekafkaplayground/ReactivekafkaplaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dthunn.reactivekafkaplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactivekafkaplaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
